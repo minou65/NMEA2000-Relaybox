378b63f7371e0c0a31511aad3a4e88c69f63d505
Andy <azogg65@bluewin.ch>
2024-10-26 09:52:54 +0200

    Remove unused N2kTimer include in CZoneSwitches.h

--- src/CZoneSwitches.h
+++ src/CZoneSwitches.h
@@ -10,27 +10,12 @@
 #endif
 
 #include <N2kMessages.h>
-#include <N2kTimer.h>
 
 void CZoneSetup();
 void CZoneOpen();
 void CZoneLoop();
 
 void CZoneHandleMessage(const tN2kMsg& N2kMsg);
-
-void ParseCZoneMFDSwitchChangeRequest65280(const tN2kMsg& N2kMsg);
-
-void SendCZoneSwitchChangeAck65283(unsigned char CzSwitchBankSerialNum);
-
-void SendCZoneSwitchHeartbeat65284(unsigned char CzSwitchBankSerialNum);
-void ParseMDF65284(const tN2kMsg& N2kMsg);
-
-void ParseMDF65288(const tN2kMsg& N2kMsg);
-
-void SendCZoneSendConfigToMFD65290(unsigned char CzSwitchBankSerial, uint8_t CZoneConfig0, uint8_t CZoneConfig1, uint8_t CZoneConfig2);
-void ParseCZoneConfigRequest65290(const tN2kMsg& N2kMsg);
-
-void SendCZoneSwitchStateBroadcast130817(unsigned char CzSwitchBankSerialNum);
 
 void SendCZoneSwitchState127501(unsigned char DeviceInstance);
 
