84c0b73a69f13f9d5acba2fc233003fb9f467ea4
Andy <azogg65@bluewin.ch>
2024-10-27 09:02:11 +0100

    Update switch bank address parameter name

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_PATCH 2
+#define VERSION_PATCH 3
 
 // Automatically generated build version. Do not modify.
-#define VERSION_BUILD 326
+#define VERSION_BUILD 327
 #define VERSION_DATE "2024-10-27"
-#define VERSION_TIME "07:00:43"
-#define VERSION "1.0.2.326"
-#define VERSION_STR "1.0.2.326 (2024-10-27 07:00:43)"
+#define VERSION_TIME "07:39:27"
+#define VERSION "1.0.3.327"
+#define VERSION_STR "1.0.3.327 (2024-10-27 07:39:27)"
