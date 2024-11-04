71d8cb6dbeb1f26ca40a9a86fee231dcde5b5e8f
Andy <azogg65@bluewin.ch>
2024-11-04 06:57:37 +0100

    build version

--- src/version.h
+++ src/version.h
@@ -3,8 +3,8 @@
 #define VERSION_PATCH 4
 
 // Automatically generated build version. Do not modify.
-#define VERSION_BUILD 337
-#define VERSION_DATE "2024-10-28"
-#define VERSION_TIME "13:51:49"
-#define VERSION "1.0.4.337"
-#define VERSION_STR "1.0.4.337 (2024-10-28 13:51:49)"
+#define VERSION_BUILD 351
+#define VERSION_DATE "2024-11-02"
+#define VERSION_TIME "17:01:25"
+#define VERSION "1.0.4.351"
+#define VERSION_STR "1.0.4.351 (2024-11-02 17:01:25)"
