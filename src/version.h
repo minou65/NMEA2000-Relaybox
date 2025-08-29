1592cfc7adf238cb1c7017be8daec43d8729a292
Andy <azogg65@bluewin.ch>
2025-08-29 16:25:49 +0200

    Update version information to reflect the latest build and date

---
+++ src/version.h
@@ -0,0 +1,10 @@
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 1
+#define VERSION_PATCH 4
+
+// Automatically generated build version. Do not modify.
+#define VERSION_BUILD 409
+#define VERSION_DATE "2025-08-29"
+#define VERSION_TIME "16:25:12"
+#define VERSION "1.1.4.409"
+#define VERSION_STR "1.1.4.409 (2025-08-29 16:25:12)"
