6d35da69bf6ada4d65bd5839ec11a179a14f50b8
Andy <azogg65@bluewin.ch>
2025-08-29 15:55:53 +0200

    Refactor test mode implementation and remove watchdog timer configuration

--- src/NMEA2000-Relaybox.ino
+++ src/NMEA2000-Relaybox.ino
@@ -1,20 +1,28 @@
 
-
-#include <esp_task_wdt.h>
+#define TESTMODE 0 // Enable test mode for relays (only first run, then normal operation). Set to 0 to disable test mode.
 
 #include "common.h"
 #include "webhandling.h"
 #include "version.h"
 #include "NMEAHandling.h"
 
+#include "neotimer.h"
+
+
+#if TESTMODE == 1
+
+// Test mode variables
+bool relayTestActive = true;
+uint8_t relayTestIndex = 0;
+bool relayTestTurnOff = false;
+Neotimer relayTestTimer = Neotimer(1000); // 1 second timer
+
+#endif // TESTMODE == 1
+
+
+
 bool SaveConfiguration = false;
 bool ChangedConfiguration = false;
-
-// Configuration for the Watchdog Timer
-esp_task_wdt_config_t wdt_config = {
-    .timeout_ms = 5000, // Timeout in milliseconds
-    .trigger_panic = true // Trigger panic if the Watchdog Timer expires
-};
 
 char Version[] = VERSION_STR; // Manufacturer's Software version code
 
@@ -28,55 +36,98 @@ void setup() {
     Serial.printf("Firmware version:%s\n", Version);
 
     webinit();
-
 	N2kBegin();
 
-    // Initialize the Watchdog Timer
-    esp_task_wdt_init(&wdt_config);
-    esp_task_wdt_add(NULL); //add current thread to WDT watch
-
     Serial.println("device initilized");
+
+#if TESTMODE == 1
+    relayTestTimer.start();
+#endif // TESTMODE == 1
+
 }
 
 void loop() {
+  
     webLoop();
-
-	N2kLoop();
-
+    N2kLoop();
     ChangedConfiguration = false;
 
-	uint8_t output_ = 0;
+#if TESTMODE == 1
+    // Relay test loop
+    if (relayTestActive) {
+        if (!relayTestTurnOff) {
+            if (relayTestTimer.done()) {
+                // Turn on next relay
+                SetSwitchStatus(relayTestIndex, true);
+                relayTestIndex++;
+                relayTestTimer.start();
+
+                // Count active relays
+                uint8_t activeCount = 0;
+                Relay* relay_ = &Relay1;
+                while (relay_ != nullptr) {
+                    if (relay_->isActive()) activeCount++;
+                    relay_ = (Relay*)relay_->getNext();
+                }
+
+                // If all relays have been turned on, prepare to turn all off
+                if (relayTestIndex >= activeCount) {
+                    relayTestTurnOff = true;
+                    relayTestTimer.start();
+                }
+            }
+        }
+        else {
+            if (relayTestTimer.done()) {
+                // Turn all relays off
+                uint8_t idx = 0;
+                Relay* relay_ = &Relay1;
+                while (relay_ != nullptr) {
+                    if (relay_->isActive()) {
+                        SetSwitchStatus(idx, false);
+                    }
+                    relay_ = (Relay*)relay_->getNext();
+                    idx++;
+                }
+                relayTestActive = false; // Stop test mode after one run
+            }
+        }
+        return; // Skip normal relay processing during test
+    }
+#endif // TESTMODE == 1
+
+    uint8_t output_ = 0;
     Relay* relay_ = &Relay1;
     while (relay_ != nullptr) {
-		if (relay_->isActive()) {
-
-           if (relay_->isTimerDone()) {
-				SendSwitchStatus(output_, false);
-		   }
-           relay_->process();
-		}
-		relay_ = (Relay*)relay_->getNext();
-		output_++;
-	}
-
-    esp_task_wdt_reset();
+        if (relay_->isActive()) {
+            if (relay_->isTimerDone()) {
+                SendSwitchStatus(output_, false);
+            }
+            relay_->process();
+        }
+        relay_ = (Relay*)relay_->getNext();
+        output_++;
+    }
 }
 
 void SetSwitchStatus(uint8_t output, bool ItemStatus) {
     Relay* relay_ = &Relay1;
-	uint8_t relayIndex_ = 0;
+    uint8_t relayIndex_ = 0;
     while (relay_ != nullptr) {
         if (relay_->isActive()) {
-			if (relayIndex_ == output) {
-                if (ItemStatus) {
-                    relay_->On();
-                }
-                else {
-                    relay_->Off();
+            if (relayIndex_ == output) {
+                if (relay_->Status() != (ItemStatus ? N2kOnOff_On : N2kOnOff_Off)) {
+                    if (ItemStatus) {
+                        relay_->On();
+                    }
+                    else {
+                        relay_->Off();
+                    }
+                    SendSwitchStatus(output, ItemStatus);
                 }
             }
-		}
-		relay_ = (Relay*)relay_->getNext();;
-		relayIndex_++;
-	}
+        }
+        relay_ = (Relay*)relay_->getNext();
+        relayIndex_++;
+    }
 }
--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 1
-#define VERSION_PATCH 1
+#define VERSION_PATCH 2
 
 // Automatically generated build version. Do not modify.
-#define VERSION_BUILD 356
-#define VERSION_DATE "2025-05-30"
-#define VERSION_TIME "07:24:19"
-#define VERSION "1.1.1.356"
-#define VERSION_STR "1.1.1.356 (2025-05-30 07:24:19)"
+#define VERSION_BUILD 405
+#define VERSION_DATE "2025-08-29"
+#define VERSION_TIME "15:51:49"
+#define VERSION "1.1.2.405"
+#define VERSION_STR "1.1.2.405 (2025-08-29 15:51:49)"
